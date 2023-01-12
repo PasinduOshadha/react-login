a0ddeb4f174c22228534cd9f1267ef87a5908fa1
Pasindu Oshadha <mailbox.pasindu@gmail.com>
2023-01-12 15:40:34 +0530

    login and register pagse added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/navbar/Navbar';
+import Login from './login/Login';
+
 
 function App() {
+  
+    
+  
   return (
     <div className="App">
-      <Navbar />
-      <div className="flex flex-row justify-center gap-5 mt-7">
-        <div className="basis-1/4 border rounded shadow-sm bg-slate-50 p-5">
-          <h3 className="text-xl font-semibold">This is column</h3>
-          <p>lorem ipsum dollar sit amet.</p>
-        </div>
-        <div className="basis-1/4 border rounded shadow-sm bg-slate-50 p-5">
-          <h3 className="text-xl font-semibold">This is column</h3>
-          <p>lorem ipsum dollar sit amet.</p>
-        </div>
-        <div className="basis-1/4 border rounded shadow-sm bg-slate-50 p-5">
-          <h3 className="text-xl font-semibold">This is column</h3>
-          <p>lorem ipsum dollar sit amet.</p>
-        </div>
-      </div>
+      <Login />
     </div>
   );
 }
